cedd26b657d6e530aa2b54284987b076a09834f8
JoseBarrera <chapixjoestar04@gmail.com>
2025-04-16 16:06:14 -0500

    Viva el Milo

---
+++ main.cpp
@@ -0,0 +1,8 @@
+/**
+ * @authors Maria Lucia Castillo y Jose Barrera Ramos
+ *
+ * Especificaciones Memoria Cache:
+ * - Asociativa 2 Vias
+ * - Politica Reemplazo LFU
+ * - Politica Actualizacion WRITE BACK
+ */
