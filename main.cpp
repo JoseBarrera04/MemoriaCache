eeaed6e7510c5d4eb89cfdbb857ec8a971b94f3f
JoseBarrera <chapixjoestar04@gmail.com>
2025-04-19 16:27:32 -0500

    Nada importante aun...X3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /**
- * @authors Maria Lucia Castillo y Jose Barrera Ramos
+ * @authors Maria Lucia Castillo Garcia y Jose Barrera Ramos
  *
  * Especificaciones Memoria Cache:
  * - Asociativa 2 Vias
@@ -7,12 +7,17 @@
  * - Politica Actualizacion WRITE BACK
  */
 
+//Librerias
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
 
 using namespace std;
+
+//Funciones
+void leerDatos();
+void actualizarDatos();
 
 /**
  * Main Principal
@@ -22,5 +27,14 @@ int main() {
     return 0;
 }
 
+/**
+ *
+ */
 void leerDatos() {
+}
+
+/**
+ *
+ */
+void actualizarDatos() {
 }
